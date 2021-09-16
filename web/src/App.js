2a8a8afbd38cbfd83b82b2a7518d451456e5b6b2
hetjagani <hetpatel572@gmail.com>
2021-09-16 08:57:40 -0700

    initialized basic web frontend project

---
+++ web/src/App.js
@@ -0,0 +1,26 @@
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+import {StatefulInput} from 'baseui/input';
+
+const engine = new Styletron();
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
+
+function App() {
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Centered>
+          <StatefulInput />
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
+  );
+}
+
+export default App;
