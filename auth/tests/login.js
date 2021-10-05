4c03475a499e11d85e12037a7a8d38376fe7c3e7
hetjagani <hetpatel572@gmail.com>
2021-10-05 16:02:52 -0700

    auth: written testcases for auth service

--- auth/tests/login.js
+++ auth/tests/login.js
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 
 let app;
 describe('POST /auth/signup', () => {
-  beforeEach((done) => {
+  before((done) => {
     require('../config');
     const { initDB } = require('../db');
     initDB()
@@ -40,6 +40,45 @@ describe('POST /auth/signup', () => {
         } else {
           res.should.have.status(200);
           res.body.should.have.property('token');
+        }
+        done();
+      });
+  });
+
+  let token = '';
+  it('it should login with the new user', (done) => {
+    const data = {
+      email: 'testuser@gmail.com',
+      password: 'Password123',
+    };
+    chai
+      .request(app)
+      .post('/auth/login')
+      .send(data)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.should.have.status(200);
+          res.body.should.have.property('token');
+          token = res.body.token;
+        }
+        done();
+      });
+  });
+
+  it('it should validate the token', (done) => {
+    chai
+      .request(app)
+      .get('/auth/validate')
+      .query({ token })
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.should.have.status(200);
+          res.body.should.have.property('valid').eql(true);
+          res.body.should.have.property('role').eql('customer');
         }
         done();
       });
